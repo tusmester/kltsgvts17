8ed0c815030dae83fc1585470ffc75979a15b5e8
tusmester <tusmester@gmail.com>
2020-02-22 06:26:07 +0100

    Add kepviselok 2019 data and layout.

---
+++ src/kltsgvts17/Kltsgvts17.Web/wwwroot/js/datatables-2019-kepviselok.js
@@ -0,0 +1,17 @@
+﻿// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    $('#dataTableKepviselok').DataTable({
+        ajax: '/data/2019/kepviselok.json',
+        lengthChange: false,
+        //paging: false,
+        //searching: false,
+        //ordering: false,
+        //info: false
+        "columns": [            
+            { data: "valasztokerulet", title: "Választókerület", "width": "8%" },
+            { data: "cimzettek", title: "Címzett", className: "dt-left" },
+            { data: "feladat", title: "Feladat", className: "dt-left" },
+            { data: "osszeg", title: "Összeg", "width": "20%", render: $.fn.dataTable.render.number(',', '.', 0, '', ' Ft') }
+        ]
+    });
+});
