8de4c4c3ba9404514db52154f756a5f90236c1b0
tusmester <tusmester@gmail.com>
2020-02-29 20:29:47 +0100

    Add kepviselo details.

--- src/kltsgvts17/Kltsgvts17.Web/Models/Kepviselo.cs
+++ src/kltsgvts17/Kltsgvts17.Web/Models/Kepviselo.cs
@@ -5,5 +5,9 @@
         public string Nev { get; set; }
         public string KepUrl { get; set; }
         public string Szerepkor { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public string FacebookUrl { get; set; }
+        public string Eletrajz { get; set; }
     }
 }
